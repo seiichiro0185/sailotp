ad5a7c8020c81e7c40bd9127605cb557e9e4333a
Johan Heikkilä <johan.heikkila@gmail.com>
2017-11-06 23:54:58 +0200

    Added Finnish translation

---
+++ i18n/fi.ts
@@ -0,0 +1,349 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi_FI">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="68"/>
+        <source>A Simple Sailfish OTP Generator
+(RFC 6238/4226 compatible)</source>
+        <translation>Yksinkertainen Sailfish OTP-generaattori
+(RFC 6238/4226-yhteensopiva)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="77"/>
+        <source>Copyright: Stefan Brand
+License: BSD (3-clause)</source>
+        <translation>Copyright: Stefan Brand
+Lisenssi: BSD (3-clause)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="95"/>
+        <source>SailOTP uses the following third party libs:</source>
+        <translation>SailOTP käyttää seuraavat kolmannen osapuolen kirjastot:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="106"/>
+        <source>Contributors:</source>
+        <translation>Avustajat:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="106"/>
+        <source>Support</source>
+        <translation>tuki</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="117"/>
+        <source>Translators:</source>
+        <translation>Kääntäjät:</translation>
+    </message>
+</context>
+<context>
+    <name>AddOTP</name>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="70"/>
+        <source>Show QR-Code</source>
+        <translation>Näytä QR-koodi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="73"/>
+        <source>Can&apos;t create QR-Code from incomplete settings!</source>
+        <translation>QR-koodin luonti epäonnistui puutteellisten asetuksien takia!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="86"/>
+        <source>Save</source>
+        <translation>Tallenna</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="86"/>
+        <source>Add</source>
+        <translation>Lisää</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="91"/>
+        <source>Type</source>
+        <translation>Tyyppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="93"/>
+        <source>Time-based (TOTP)</source>
+        <translation>Aikapohjainen (TOTP)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="94"/>
+        <source>Counter-based (HOTP)</source>
+        <translation>Laskentapohjainen (HOTP)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="95"/>
+        <source>Steam Guard</source>
+        <translation>Steam Guard</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="101"/>
+        <source>Title</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="102"/>
+        <source>Title for the OTP</source>
+        <translation>OTP:n nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="114"/>
+        <source>Secret (at least 16 characters)</source>
+        <translation>Salaisuus (vähintään 16 merkkiä)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="116"/>
+        <source>Secret OTP Key</source>
+        <translation>Salainen OTP-avain</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="127"/>
+        <source>Length</source>
+        <translation>Pituus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="129"/>
+        <source>Length of the Token</source>
+        <translation>Tunnuksen pituus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="141"/>
+        <location filename="../qml/pages/AddOTP.qml" line="143"/>
+        <source>Time Derivation (Seconds)</source>
+        <translation>Aikapoikkeama (sekunneissa)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="155"/>
+        <source>Next Counter Value</source>
+        <translation>Seuraava laskenta-arvo</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="157"/>
+        <source>Next Value of the Counter</source>
+        <translation>Laskurin seuraava arvo</translation>
+    </message>
+</context>
+<context>
+    <name>ExportPage</name>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="63"/>
+        <source>File already exists, choose &quot;Overwrite existing&quot; to overwrite it.</source>
+        <translation>Tiedosto on jo olemassa. Korvaa vanha tiedosto valitsemalla &quot;Ylikirjoita&quot;.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="72"/>
+        <source>Given file does not exist!</source>
+        <translation>Tiedosto ei ole olemassa!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="91"/>
+        <location filename="../qml/pages/ExportPage.qml" line="107"/>
+        <source>Export</source>
+        <translation>Eksportoi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="91"/>
+        <location filename="../qml/pages/ExportPage.qml" line="107"/>
+        <source>Import</source>
+        <translation>Importoi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="114"/>
+        <source>Filename</source>
+        <translation>Tiedoston nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="115"/>
+        <source>File to import</source>
+        <translation>Importoitava tiedosto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="115"/>
+        <source>File to export</source>
+        <translation>Eksportoitava tiedosto</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="128"/>
+        <source>Overwrite existing</source>
+        <translation>Ylikirjoita</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="134"/>
+        <source>Password</source>
+        <translation>Salasana</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="135"/>
+        <source>Password for the file</source>
+        <translation>Tiedoston salasana</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="148"/>
+        <source>Passwords don&apos;t match!</source>
+        <translation>Salasanat eivät täsmää!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="148"/>
+        <source>Passwords match!</source>
+        <translation>Salasanat täsmäävät!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="149"/>
+        <source>Repeated Password for the file</source>
+        <translation>Tiedoston salasana uudelleen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="173"/>
+        <source>Here you can Import Tokens from a file. Put in the file location and the password you used on export. Pull left to start the import.</source>
+        <translation>Voit importata tunnuksia tiedostosta. Anna tiedoston polku ja salasana jonka annoit eksportoitaessa. Pyyhkäise vasemmalle aloittaaksesi importin.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="189"/>
+        <source>Here you can export Tokens to a file. The exported file will be encrypted with AES-256-CBC and Base64 encoded. Choose a strong password, the file will contain the secrets used to generate the Tokens for your accounts. Pull left to start the export.</source>
+        <translation>Voit eksportoida tunnukset tiedostoon. Eksportoitu tiedosto enkryptataan AES-256-CBC- ja Base64-salauksella. Valitse vahva salasana, koska tiedostosta voidaan kääntää tiliesi tunnukset. Pyyhkäise vasemmalle aloittaaksesi eksportin.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="211"/>
+        <source>Error writing to file </source>
+        <translation>Virhe tiedoston kirjoituksessa </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="213"/>
+        <source>Token Database exported to </source>
+        <translation>Tunnus-tietokanta eksportoitu polkuun </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="216"/>
+        <source>Could not encrypt tokens. Error: </source>
+        <translation>Tunnuksia ei voitu enkryptata. Virhe: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="219"/>
+        <source>Could not read tokens from Database</source>
+        <translation>Tunnuksia ei voitu lukea tietokannasta</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="230"/>
+        <source>Tokens imported from </source>
+        <translation>Tunnukset importoitu polusta </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="235"/>
+        <source>Unable to decrypt file, did you use the right password?</source>
+        <translation>Tiedoston dekryptaus epäonnistui. Oliko salasana oikea?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="238"/>
+        <source>Could not read from file </source>
+        <translation>Tiedoston lukeminen epäonnistui </translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="96"/>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="100"/>
+        <source>Export / Import</source>
+        <translation>Eksportoi / Importoi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="104"/>
+        <source>Add Token</source>
+        <translation>Lisää tunnus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="134"/>
+        <source>Nothing here</source>
+        <translation>Tyhjä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="135"/>
+        <source>Pull down to add a OTP</source>
+        <translation>Vedä alaspäin lisääksesi OTP:n</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="146"/>
+        <source>Deleting</source>
+        <translation>Poistaa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="162"/>
+        <source>Token for </source>
+        <translation>Tunnus </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="162"/>
+        <source> copied to clipboard</source>
+        <translation> kopioitu leikepöydälle</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="230"/>
+        <source>Move up</source>
+        <translation>Siirrä ylös</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="235"/>
+        <source>Move down</source>
+        <translation>Siirrä alas</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="240"/>
+        <source>Edit</source>
+        <translation>Muokkaa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="246"/>
+        <source>Delete</source>
+        <translation>Poista</translation>
+    </message>
+</context>
+<context>
+    <name>QRPage</name>
+    <message>
+        <location filename="../qml/pages/QRPage.qml" line="74"/>
+        <source>Can&apos;t create QR-Code from incomplete settings!</source>
+        <translation>QR-koodin luonti ei onnistu puutteellisten asetusten takia!</translation>
+    </message>
+</context>
+<context>
+    <name>ScanOTP</name>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="54"/>
+        <source>Can&apos;t access temporary directory</source>
+        <translation>Pääsy temp-hakemistoon on estetty</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="64"/>
+        <source>Add manually</source>
+        <translation>Lisää käsin</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="71"/>
+        <source>Scan Code</source>
+        <translation>Skannaa koodi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="71"/>
+        <source>Scanning...</source>
+        <translation>Skannaa...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="101"/>
+        <source>No valid Token data found.</source>
+        <translation>Voimassa olevaa tunnustietoa ei löytynyt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="142"/>
+        <source>Tap the picture to start / stop scanning. Pull down to add Token manually.</source>
+        <translation>Käynnistä / lopeta skannaus painamalla kuvaa. Vedä alaspäin lisääksesi tunnuksen käsin.</translation>
+    </message>
+</context>
+</TS>
