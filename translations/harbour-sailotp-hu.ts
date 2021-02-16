8cdfb544c46ce06d13df0c35be0723462bd2582f
1Zgp <36508793+1Zgp@users.noreply.github.com>
2021-02-16 09:30:30 +0200

    Add files via upload

---
+++ translations/harbour-sailotp-hu.ts
@@ -0,0 +1,426 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="87"/>
+        <source>Online Manual</source>
+        <translation>Online kézikönyv</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="94"/>
+        <source>Source Code &amp; Issue Tracker</source>
+        <translation>Forráskód &amp; Hibakövetés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="111"/>
+        <source>Copyright</source>
+        <translation>Szerzői jog</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="120"/>
+        <source>License</source>
+        <translation>Licenc</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="120"/>
+        <source>BSD (3-clause)</source>
+        <translation>BSD (3-clause)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="133"/>
+        <source>Contributors:</source>
+        <translation>Közreműködők:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="139"/>
+        <source>SteamGuard support</source>
+        <translation>SteamGuard támogatás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="146"/>
+        <source>Search</source>
+        <translation>Keresés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="240"/>
+        <source>SailOTP uses the following third party libs:</source>
+        <translation>A SailOTP a következő harmadik féltől származó könyvtárakat használja:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="70"/>
+        <source>A simple Sailfish OTP generator</source>
+        <translation>Egy egyszerű OTP generátor Sailfish OS-re</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="78"/>
+        <source>(RFC 6238/4226 compatible)</source>
+        <translation>(RFC 6238/4226 kompatibilis)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="162"/>
+        <source>Translators:</source>
+        <translation>Fordítók:</translation>
+    </message>
+</context>
+<context>
+    <name>AddOTP</name>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="69"/>
+        <source>Show QR-Code</source>
+        <translation>QR-kód megjelenítése</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="72"/>
+        <location filename="../qml/pages/AddOTP.qml" line="85"/>
+        <source>Can&apos;t create QR-Code from incomplete settings!</source>
+        <translation>Nem hozható létre QR-kód hiányos beállításokból!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="98"/>
+        <source>Add</source>
+        <translation>Hozzáadás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="98"/>
+        <source>Save</source>
+        <translation>Mentés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="103"/>
+        <source>Type</source>
+        <translation>Típus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="105"/>
+        <source>Time-based (TOTP)</source>
+        <translation>Idő-alapú (TOTP)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="106"/>
+        <source>Counter-based (HOTP)</source>
+        <translation>Számláló-alapú (HOTP)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="107"/>
+        <source>Steam Guard</source>
+        <translation>Steam Guard</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="113"/>
+        <source>Title</source>
+        <translation>Cím</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="114"/>
+        <source>Title for the OTP</source>
+        <translation>Az OTP címe</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="126"/>
+        <source>Secret (at least 16 characters)</source>
+        <translation>Jelszó (legalább 16 karakter)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="128"/>
+        <source>Secret OTP Key</source>
+        <translation>Titkos OTP-kulcs</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="142"/>
+        <source>Length</source>
+        <translation>Hosszúság</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="144"/>
+        <source>Length of the Token</source>
+        <translation>A token hosszúsága</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="156"/>
+        <location filename="../qml/pages/AddOTP.qml" line="158"/>
+        <source>Time Derivation (Seconds)</source>
+        <translation>Időlevezetés (Másodperc)</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="170"/>
+        <source>Next Counter Value</source>
+        <translation>Következő számlálóérték</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddOTP.qml" line="172"/>
+        <source>Next Value of the Counter</source>
+        <translation>A számláló következő értéke</translation>
+    </message>
+</context>
+<context>
+    <name>ExportPage</name>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="63"/>
+        <source>File already exists, choose &quot;Overwrite existing&quot; to overwrite it.</source>
+        <translation>A fájl már létezik, válaszd a &quot;Meglévő felülírása&quot; lehetőséget a felülíráshoz.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="72"/>
+        <source>Given file does not exist!</source>
+        <translation>Az adott fájl nem létezik!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="91"/>
+        <location filename="../qml/pages/ExportPage.qml" line="107"/>
+        <source>Export</source>
+        <translation>Exportálás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="91"/>
+        <location filename="../qml/pages/ExportPage.qml" line="107"/>
+        <source>Import</source>
+        <translation>Importálás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="114"/>
+        <source>Filename</source>
+        <translation>Fájlnév</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="115"/>
+        <source>File to import</source>
+        <translation>Importálandó fájl</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="115"/>
+        <source>File to export</source>
+        <translation>Exportálandó fájl</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="128"/>
+        <source>Overwrite existing</source>
+        <translation>Meglévő felülírása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="134"/>
+        <source>Password</source>
+        <translation>Jelszó</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="135"/>
+        <source>Password for the file</source>
+        <translation>Jelszó a fájlhoz</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="148"/>
+        <source>Passwords don&apos;t match!</source>
+        <translation>A jelszavak nem egyeznek!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="148"/>
+        <source>Passwords match!</source>
+        <translation>A jelszavak egyeznek!</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="149"/>
+        <source>Repeated Password for the file</source>
+        <translation>Megismételt jelszó a fájlhoz</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="173"/>
+        <source>Here you can Import Tokens from a file. Put in the file location and the password you used on export. Pull left to start the import.</source>
+        <translation>Itt tudsz tokeneket importálni egy fájlból. Írd be a fájl helyét, és az exportáláskor használt jelszót. Húzd balra az importálás indításához.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="189"/>
+        <source>Here you can export Tokens to a file. The exported file will be encrypted with AES-256-CBC and Base64 encoded. Choose a strong password, the file will contain the secrets used to generate the Tokens for your accounts. Pull left to start the export.</source>
+        <translation>Itt tudsz tokeneket exportálni egy fájlba. Az exportált fájl AES-256-CBC és Base64 kódolással lesz ellátva. Válassz egy erős jelszót, a fájl a fiókjaidoz létrehozandó tokenehez tartozó jelszavakat fogja tartalmazni. Húzd balra az exportálás indításához.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="189"/>
+        <source>To view the content of the export file outside of SailOTP use the following openssl command:</source>
+        <translation>Az exportfájl tartalmának a SailOTP-n kívüli megtekintéséhez használd a következő openssl parancsokat:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="211"/>
+        <source>Error writing to file </source>
+        <translation>Hiba a fájlba íráskor </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="213"/>
+        <source>Token Database exported to </source>
+        <translation>Tokenadatbázis exportálva ide: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="216"/>
+        <source>Could not encrypt tokens. Error: </source>
+        <translation>A tokenek nem titkosíthatók. Hiba: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="219"/>
+        <source>Could not read tokens from Database</source>
+        <translation>A tokenek nem olvashatók az adatbázisból</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="230"/>
+        <source>Tokens imported from </source>
+        <translation>Tokenek importálva innen: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="235"/>
+        <source>Unable to decrypt file, did you use the right password?</source>
+        <translation>Nem lehet dekódolni a fájlt, a helyes jelszót használtad?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ExportPage.qml" line="238"/>
+        <source>Could not read from file </source>
+        <translation>Nem olvasható a fájlból </translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="109"/>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="113"/>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="118"/>
+        <source>Export / Import</source>
+        <translation>Exportálás / Importálás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="122"/>
+        <source>Add Token</source>
+        <translation>Token hozzáadása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="136"/>
+        <source>Nothing here</source>
+        <translation>Itt nincs semmi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="137"/>
+        <source>Pull down to add a OTP</source>
+        <translation>OTP hozzáadásához húzd le</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="178"/>
+        <source>Search</source>
+        <translation>Keresés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="198"/>
+        <source>Deleting</source>
+        <translation>Törlés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="219"/>
+        <location filename="../qml/pages/MainView.qml" line="293"/>
+        <source>Token for </source>
+        <translation>A tokenje ennek: </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="219"/>
+        <location filename="../qml/pages/MainView.qml" line="293"/>
+        <source> copied to clipboard</source>
+        <translation> a vágólapra másolva</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="289"/>
+        <source>Copy to Clipboard</source>
+        <translation>Másolás a vágólapra</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="297"/>
+        <source>Show Token as QR-Code</source>
+        <translation>Token megjelenítése QR-kódként</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="302"/>
+        <source>Move up</source>
+        <translation>Mozgatás felfelé</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="307"/>
+        <source>Move down</source>
+        <translation>Mozgatás lefelé</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="312"/>
+        <source>Edit</source>
+        <translation>Szerkesztés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainView.qml" line="318"/>
+        <source>Delete</source>
+        <translation>Törlés</translation>
+    </message>
+</context>
+<context>
+    <name>QRPage</name>
+    <message>
+        <location filename="../qml/pages/QRPage.qml" line="74"/>
+        <source>Can&apos;t create QR-Code from an empty String</source>
+        <translation>Üres bejegyzésből nem hozható létre QR-kód</translation>
+    </message>
+</context>
+<context>
+    <name>ScanOTP</name>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="54"/>
+        <source>Can&apos;t access temporary directory</source>
+        <translation>Az ideiglenes mappa nem érhető el</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="64"/>
+        <source>Add manually</source>
+        <translation>Kézi hozzáadás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="71"/>
+        <source>Scan Code</source>
+        <translation>Kód szkennelése</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="71"/>
+        <source>Scanning...</source>
+        <translation>Szkennelés...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="96"/>
+        <source>No valid Token data found.</source>
+        <translation>Nem található érvényes token-adat.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ScanOTP.qml" line="137"/>
+        <source>Tap the picture to start / stop scanning. Pull down to add Token manually.</source>
+        <translation>Érintsd meg a képet a szkennelés elindításához / megállításához. Húzd le a token kézi hozzáadásához.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="50"/>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="52"/>
+        <source>Behaviour</source>
+        <translation>Viselkedés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="56"/>
+        <source>Show Token as QR on Tap</source>
+        <translation>Token megjelenítése QR-ként érintéskor</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="68"/>
+        <source>Hide Tokens and Reveal on Tap</source>
+        <translation>Tokenek elrejtése és megjelenítése érintéskor</translation>
+    </message>
+</context>
+</TS>
