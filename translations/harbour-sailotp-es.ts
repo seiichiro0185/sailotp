a6044de2b1bd8b90f73a51529b85a765c0623960
Stefan Brand <seiichiro@seiichiro0185.org>
2021-06-26 16:03:23 +0200

    Prepare Release 1.9.4

--- translations/harbour-sailotp-es.ts
+++ translations/harbour-sailotp-es.ts
@@ -49,7 +49,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="247"/>
+        <location filename="../qml/pages/About.qml" line="153"/>
+        <source>Icon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="261"/>
         <source>SailOTP uses the following third party libs:</source>
         <translation>SailOTP usa las siguientes librerías de terceros:</translation>
     </message>
@@ -59,7 +64,7 @@
         <translation>Contribuidor:</translation>
     </message>
     <message>
-        <location filename="../qml/pages/About.qml" line="162"/>
+        <location filename="../qml/pages/About.qml" line="169"/>
         <source>Translators:</source>
         <translation>Traductores:</translation>
     </message>
